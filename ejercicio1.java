eb56551cf0bc852f2f9f6a3c4a1035cbadad3813
unknown <sealkevinboyabad@gmail.com>
2024-09-20 13:32:14 +0200

    ejercicio1

---
+++ ejercicio1.java
@@ -0,0 +1,15 @@
+public class ejercicio1 {
+    public static void main(String[] args) {
+        int numeroEntero = 42;
+        float numeroDecimal = 3.14f;
+        char caracter = 'A';
+        boolean valorLogico = true;
+        String texto = "Hola, Mundo";
+
+        System.out.println("Número entero: " + numeroEntero);
+        System.out.println("Número decimal: " + numeroDecimal);
+        System.out.println("Caracter: " + caracter);
+        System.out.println("ValorLógico: " + valorLogico);
+        System.out.println("Texto: " + texto);
+    }   
+}
