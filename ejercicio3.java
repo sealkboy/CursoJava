3588feb29eaf6942c7bdc5fd93b3fa6c211ff32f
unknown <sealkevinboyabad@gmail.com>
2024-09-20 13:48:00 +0200

    ejercicio3

--- ejercicio3.java
+++ ejercicio3.java
@@ -1,3 +1,16 @@
 public class ejercicio3 {
-    
+    public static void main(String[] args) {
+        int a = 5; 
+        int b = 10; 
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+
+        a = a + b; 
+        b = a - b; 
+        a = a - b; 
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+    }
 }
